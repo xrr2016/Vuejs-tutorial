326421590eaf373fc37ee55cc5f45529691598e2
xrr2016 <13286904669@163.com>
2016-12-02 23:29:23 +0800

    :hankey: add

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="jumbotron">
+    <h1>任务追踪</h1>
+    <p>
+      <strong>
+        <router-link to='/time-entries'>创建一个任务</router-link>
+      </strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{}
+  },
+  components:{}
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 
 /* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/',
+    component:App
+  }
+]
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   el: '#app',
   template: '<App/>',
