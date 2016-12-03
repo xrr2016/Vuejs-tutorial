05abef5114d6abfcc9ca0a8061cd748475e28ac7
xrr2016 <13286904669@163.com>
2016-12-03 23:26:07 +0800

    :hankey: add

---
+++ src/components/404.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="">
+    <p>404! ORZ!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="panel panel-deafult">
+    <div class="panel-heading">
+      <h1 class="text-center">已有时长</h1>
+    </div>
+
+  <div class="panel-body">
+    <h1 class="text-center">{{ time }}小时</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name:'Sidebar',
+  computed:{
+    time(){
+      return this.$store.state.totalTime
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
