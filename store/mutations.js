5ab4c34f25cebde80ddfae76f752a3653e8ada85
xrr2016 <13286904669@163.com>
2016-12-06 19:21:24 +0800

    :hankey: add

---
+++ store/mutations.js
@@ -0,0 +1,22 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.ADD_TOTAL_TIME](state,time){
+    state.tatolTime  = state.totalTime + time
+  },
+  [types.DEC_TOTAL_TIME](state.time){
+    state.totalTime = state.totalTime - time
+  },
+  [types.SAVE_PLAN](state,plan){
+    const avatar = 'https://sfault-avatar.b0.upaiyun.com/247/437/2474377559-57ecb0ecc1038_huge256'
+    state.list.push(
+      Object.assign({
+        name:'someone',
+        avatar:avatar
+      },plan)
+    )
+  },
+  [types.DELETE_PLAN](state,idx){
+    state.list.splice(idx,1)
+  }
+}
