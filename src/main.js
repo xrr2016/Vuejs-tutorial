3d7147d1e1bae466499dd68e0a8bfd284d617dd1
xrr2016 <13286904669@163.com>
2016-12-07 21:39:40 +0800

    :hankey: edit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import store  from './store'
 import App from './App'
+
+import Home from './components/Home'
+import TimeEntries from './components/TimeEntries'
+import LogTime from './components/LogTime'
+import NotFound from './components/404'
+
+
 
 /* eslint-disable no-new */
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const routes = [
   {
     path:'/',
-    component:App
+    component: Home
+  },
+  {
+    path:'/home',
+    component: Home
+  },
+  {
+    path:'/time-entries',
+    component: TimeEntries,
+    chidren:[
+      path:'log-time',
+      component:LogTime
+    ]
+  },
+  {
+    path:'*',
+    component: NotFound
   }
 ]
 const router = new VueRouter({
@@ -17,6 +45,7 @@ const router = new VueRouter({
 
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  router,
+  store,
+  ...App,
 })
